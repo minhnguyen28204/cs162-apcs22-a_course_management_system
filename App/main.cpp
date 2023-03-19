0b382ef289970e088ba731112aa525e7cbef2b91
minhnguyen28204 <69662229+minhnguyen28204@users.noreply.github.com>
2023-03-19 17:23:09 +0700

    Add background for login screen

---
+++ App/main.cpp
@@ -0,0 +1,46 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(1250, 900), "Course Management");
+
+  sf::Texture BackgroundTexture;
+  sf::Sprite background;
+  sf::Vector2u TextureSize;  //Added to store texture size.
+  sf::Vector2u WindowSize;   //Added to store window size.
+
+  if(!BackgroundTexture.loadFromFile("Image/LOGIN.jpg"))
+  {
+    return -1;
+  }
+  else
+  {
+    TextureSize = BackgroundTexture.getSize(); //Get size of texture.
+    WindowSize = window.getSize();             //Get size of window.
+
+    float ScaleX = (float) WindowSize.x / TextureSize.x;
+    float ScaleY = (float) WindowSize.y / TextureSize.y;     //Calculate scale.
+
+    background.setTexture(BackgroundTexture);
+    background.setScale(ScaleX, ScaleY);      //Set scale.
+  }
+
+  while(window.isOpen())
+  {
+    sf::Event event;
+
+    while(window.pollEvent(event))
+    {
+        switch(event.type)
+        {
+            case sf::Event::Closed:
+                window.close();
+        }
+    }
+    window.clear();
+    window.draw(background);
+    window.display();
+ }
+}
