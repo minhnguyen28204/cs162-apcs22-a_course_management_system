22104a312f1bdda3ea872a9bad56e2bbda67ef88
Mai Xuan Bach <maixuanbach174@gmail.com>
2023-03-21 10:40:38 +0700

    create the student struct
    Mai Xuan Bach

--- App/main.cpp
+++ App/main.cpp
@@ -1,42 +1,4 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(1250, 900), "Course Management", sf::Style::Titlebar | sf::Style::Close);
 
-    sf::Texture BackgroundTexture;
-    sf::Sprite background;
-    sf::Vector2u TextureSize;  //Added to store texture size.
-    sf::Vector2u WindowSize;   //Added to store window size.
-
-    if(!BackgroundTexture.loadFromFile("Image/LOGIN.jpg")){
-        return -1;
-    }else{
-        TextureSize = BackgroundTexture.getSize(); //Get size of texture.
-        WindowSize = window.getSize();             //Get size of window.
-
-        float ScaleX = (float) WindowSize.x / TextureSize.x;
-        float ScaleY = (float) WindowSize.y / TextureSize.y;     //Calculate scale.
-
-        background.setTexture(BackgroundTexture);
-        background.setScale(ScaleX, ScaleY);
-        //Set scale.
-    }
-    cout << "Hello :D";
-
-    while(window.isOpen()){
-        sf::Event event;
-
-        while(window.pollEvent(event)){
-            switch(event.type){
-                case sf::Event::Closed:
-                    window.close();
-            }
-        }
-        window.clear();
-        window.draw(background);
-        window.display();
-    }
-}
