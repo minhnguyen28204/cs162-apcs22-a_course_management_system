25949ca91df5fc1c5bbcea2d12a6c534ba1ed0ea
minhnguyen28204 <69662229+minhnguyen28204@users.noreply.github.com>
2023-03-21 10:55:41 +0700

    Thêm Linked List Template

--- App/main.cpp
+++ App/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "Student.h"
+#include "LinkedList.hpp"
 
 int main(){
+    SLLinkedList<Student> List_Students;
+    List_Students.push_front({22125058,"Minh","Nguyen",1,"28-02-2004","11111"});
+    std::cout << List_Students.Head->data.LastName << '\n';
+    std::cout << List_Students.size() << '\n';
+    List_Students.remove([&](Student P){
+                    if (P.ID==22125058) return true;
+                    return false;
+                        });
+    std::cout << List_Students.size();
 
     return 0;
 }
