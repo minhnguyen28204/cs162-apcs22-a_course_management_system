b4b4b1a7d51dad5731ccfd160cb2528b2526e0cb
Dương Ngọc Quang Khiêm <128200532+KhimDuong@users.noreply.github.com>
2023-03-28 13:34:22 +0700

    Khiem import

--- App/Import.h
+++ App/Import.h
@@ -1,6 +1,19 @@
 #ifndef IMPORT_H
 #define IMPORT_H
+#include "DoublyLinkedList.hpp"
+#include <string>
+#include <fstream>
+#include "year.h"
+#include <Semester.h>
+#include <Course.h>
+#include <Class.h>
+using namespace std;
 
 
+//import year
+void importYear(string filename, DLinkedList <Year> &year_list);
+void importSemester(string filename, DLinkedList <Semester> &semester_list);
+void importCourse(String filename, DLinkedList <Semester> &courses_list);
+void importClass(string filename, DLinkedList <Class> &classes_list);
 
 #endif
